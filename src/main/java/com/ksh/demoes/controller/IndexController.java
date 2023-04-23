ee7cc3ec57c722d10ef523fa604ee097d0f49020
GitHubkshcode <42910366+GitHubkshcode@users.noreply.github.com>
2023-04-23 11:09:51 +0800

    init

---
+++ src/main/java/com/ksh/demoes/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.ksh.demoes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/toIndex")
+    public String toIndexPage() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/ksh/demoes/DemoWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.demoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
